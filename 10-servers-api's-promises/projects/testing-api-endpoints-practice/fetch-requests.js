f04a9e7933049d4088be9507229e5a7e0a9d8659
Giorgi <Gio.Chiaberashvili@gmail.com>
2023-03-28 15:43:46 +0400

    Testing API Endpoints

---
+++ 10-servers-api's-promises/projects/testing-api-endpoints-practice/fetch-requests.js
@@ -0,0 +1,51 @@
+/*
+Make fetch requests in the browser for each of the following tasks.
+Paste your code for fetch requests here once you finish each task.
+*/
+
+/* =============================== Phase 1 ================================ */
+/*
+  Make a request with fetch request to GET /posts and print the response
+  components to the console.
+*/
+
+// Your code here
+let myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+let raw = JSON.stringify({
+  "message": "BYE GIO!"
+});
+
+let requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'manual'
+};
+
+fetch("/posts", requestOptions)
+  .then(response => response.json())
+  .then(resBody => console.log(resBody))
+  .catch(error => console.log('error', error));
+
+
+/* =============================== Phase 2 ================================ */
+/*
+  Make a request with fetch request to POST /posts and print the response
+  components to the console.
+*/
+
+// Your code here
+fetch('/post', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    message: 'New Post!'
+  })
+})
+  .then(res => res.json())
+  .then(resBody => console.log(resBody))
+  .catch(error => console.log('error', error))
