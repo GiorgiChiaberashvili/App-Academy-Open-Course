7cef33ff5a471547a19b8baf76af03b31e65fafe
Giorgi <Gio.Chiaberashvili@gmail.com>
2023-04-13 12:21:15 +0400

    Express Route Handlers

---
+++ 13-express-&-sql/projects/express-route-handlers-practice/server/app.js
@@ -0,0 +1,61 @@
+// Phase 2
+const {
+  getAllArtists,
+  getLatestArtist,
+  getArtistByArtistId,
+  addArtist,
+  editArtistByArtistId,
+  deleteArtistByArtistId,
+  getAlbumsForLatestArtist,
+  getAlbumsByArtistId,
+  getAlbumByAlbumId,
+  addAlbumByArtistId,
+  editAlbumByAlbumId,
+  deleteAlbumByAlbumId,
+  getFilteredAlbums,
+  getSongsByArtistId,
+  getSongsByAlbumId,
+  getSongBySongId,
+  addSongByAlbumId,
+  editSongBySongId,
+  deleteSongBySongId
+} = require('./data');
+
+const express = require('express');
+const app = express();
+
+// Your code here
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Request Body:', req.body);
+  next();
+});
+
+app.get('/artists', (req, res) => {
+  res.statusCode = 200;
+  res.send(getAllArtists())
+})
+
+app.post('/artists', (req, res) => {
+  res.statusCode = 201;
+  res.send(addArtist(req.body))
+})
+
+app.get('/artists/latest', (req, res) => {
+  res.statusCode = 200;
+  res.send(getLatestArtist(req.body))
+})
+
+app.get('/artists/latest/albums', (req, res) => {
+  res.statusCode = 200;
+  res.send(getAlbumsForLatestArtist())
+})
+
+// DO NOT MODIFY
+if (require.main === module) {
+  const port = 8000;
+  app.listen(port, () => console.log('Server is listening on port', port));
+} else {
+  module.exports = app;
+}
