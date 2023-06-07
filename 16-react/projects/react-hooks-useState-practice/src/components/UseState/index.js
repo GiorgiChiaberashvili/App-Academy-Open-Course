6de6ac5c6931096b9be78b8b95dbf373a5a326b2
Giorgi <Gio.Chiaberashvili@gmail.com>
2023-06-07 13:33:58 +0400

    React Hooks UseState

---
+++ 16-react/projects/react-hooks-useState-practice/src/components/UseState/index.js
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './UseState.css';
+
+const UseState = () => {
+
+  // console.log(useState('light'));
+
+  const [theme, setTheme] = useState('light');
+  const [count, setCount] = useState(0);
+
+  const Toggletheme = () => {
+    if (theme === 'light') {
+      setTheme('dark')
+    } else {
+      setTheme('light')
+    }
+  }
+
+  return (
+    <div className={`state ${theme}`}>
+      <h1>UseState Component</h1>
+      <button onClick={() => setTheme('dark')}>Dark</button>
+      <button onClick={() => setTheme('light')}>Light</button>
+      <h2>{count}</h2>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>Increment</button>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>Decrement</button>
+      <button onClick={() => Toggletheme('dark')}>Dark</button>
+    </div>
+  );
+};
+
+
+export default UseState;
