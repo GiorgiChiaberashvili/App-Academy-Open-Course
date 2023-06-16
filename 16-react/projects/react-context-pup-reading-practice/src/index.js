aed02ea3ae9ad152e798eb04f4aca618ba441893
Giorgi <Gio.Chiaberashvili@gmail.com>
2023-06-16 15:14:17 +0400

    React Context Pup Reading

---
+++ 16-react/projects/react-context-pup-reading-practice/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { PupProvider } from './context/PupContext';
+
+ReactDOM.render(
+  <PupProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </PupProvider>,
+  document.getElementById('root')
+);
