7be3e26f83fad2abab0c420648b0b3fd708e16a3
Giorgi <Gio.Chiaberashvili@gmail.com>
2023-06-19 15:24:40 +0400

    React Context Review with Tests

---
+++ 16-react/projects/react-context-review-with-tests-long-practice/src/components/SelectedCoffeeBean.js
@@ -0,0 +1,14 @@
+import { CoffeeContext } from "../context/CoffeeContext";
+import { useContext } from "react";
+
+const SelectedCoffeeBean = () => {
+  const { coffeeBean } = useContext(CoffeeContext)
+
+  return (
+    <div className="selected-coffee">
+      <h2>Current Selection: {coffeeBean.name} </h2>
+    </div>
+  );
+}
+
+export default SelectedCoffeeBean;
