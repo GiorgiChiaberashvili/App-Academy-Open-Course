8503f764104ea5356bc464634bdb725ff67fa9cb
Giorgi <Gio.Chiaberashvili@gmail.com>
2023-06-22 13:50:11 +0400

    React Redux Store

---
+++ 17-redux-&-authentication/projects/react-redux-store-practice/src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+import "./index.css";
+
+import createStore from "./store";
+
+const store = createStore();
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
