be883eaf043071afeed2d15071132b32f46d32ff
Giorgi <Gio.Chiaberashvili@gmail.com>
2023-06-26 16:42:40 +0400

    React Redux useDispatch-useSelector

---
+++ 17-redux-&-authentication/projects/react-redux-useDispatch-useSelector-practice/src/components/ArticleList/index.js
@@ -0,0 +1,40 @@
+import { Route, Switch } from 'react-router-dom';
+import SingleArticle from '../SingleArticle';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { loadArticles } from '../../store/articleReducer';
+import { useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+
+
+const ArticleList = () => {
+  const dispatch = useDispatch();
+
+  const articles = useSelector(state => state.articleState.entries);
+
+  // console.log('state', state)
+
+  useEffect(() => {
+    dispatch(loadArticles());
+  }, [dispatch]);
+
+
+  return (
+    <div>
+      <h1>Article List</h1>
+      <ol>
+        {articles.map(({ id, title }) => (
+          <li key={id}><NavLink to={`/article/${id}`}>{title}</NavLink></li>
+        ))}
+      </ol>
+
+      <Switch>
+        <Route path='/article/:id'>
+          <SingleArticle />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
+
+export default ArticleList;
